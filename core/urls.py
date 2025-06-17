6093543175f71f823631ef58a7e7304b07cfaac0
AAMIRMMAHDI <156841411+AAMIRMMAHDI@users.noreply.github.com>
2025-06-17 14:11:33 +0330

    CLON_7

--- core/urls.py
+++ core/urls.py
@@ -28,5 +28,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('root.urls')),
     path('', include('root.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  
 
